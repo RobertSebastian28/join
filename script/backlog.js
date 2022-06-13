11f11ea3ac30ef95f5be76ad9af6b9561fb8fc2b
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-06-13 17:45:28 +0200

    test script for showTask function

--- script/backlog.js
+++ script/backlog.js
@@ -2,10 +2,10 @@ function responsiveHead() {
 
     if (window.innerWidth < 870) {
         document.getElementById('categoryDetails').classList.add('d-none');
-        document.getElementById('testId').classList.remove('d-flex');
+        document.getElementById('secondGap').classList.remove('d-flex');
     } else {
         document.getElementById('categoryDetails').classList.remove('d-none');
-        document.getElementById('testId').classList.add('d-flex');
+        document.getElementById('secondGap').classList.add('d-flex');
     }
 }
 
@@ -13,15 +13,65 @@ function responsiveHead() {
 function renderBacklog() {
     let container = document.getElementById('backlogBox');
 
-    container.innerHTML = `<div class="d-flex justify-content-start rowStyle">
-    <div class="d-flex align-items-center firstGap">
-        
-        <img src="img/user_dummy.png" class="profilePicture" id="profilePicture" alt="">
-        <div id="assignedToInfo">Beispiel1</div>
-    </div>
-    <div id="testId" class="d-flex secondGap">
-        <div id="categoryInfo">Beispiel1</div>
-        <div id="detailsInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo perferendis pariatur architecto. Saepe at voluptatem dolorum harum expedita. Dignissimos aperiam dolorem consequuntur est. Doloremque exercitationem adipisci
+    container.innerHTML += `
+    
+    <div id="backlogRow" onclick="showTask()" class="d-flex justify-content-start rowStyle">
+        <div class="d-flex align-items-center firstGap">
+
+            <img src="img/user_dummy.png" class="profilePicture" id="profilePicture" alt="">
+            <div id="assignedToInfo">Beispiel1</div>
+        </div>
+        <div id="secondGap" class="d-flex secondGap">
+            <div id="categoryInfo">Beispiel1</div>
+            <div id="detailsInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo perferendis pariatur architecto. Saepe at voluptatem dolorum harum expedita. Dignissimos aperiam dolorem consequuntur est. Doloremque exercitationem adipisci
             et officia sequi.</div>
-    </div>`
+        </div>
+    </div>
+        <div id="addToBoard" class="d-none">
+            <div>To Do</div>
+            <div>In Progress</div>
+            <div>Testing</div>
+            <div>Done</div>
+        <div>
+    `
 }
+
+function showTask() {
+    let hiddenContainer = document.getElementById('addToBoard');
+
+    if (hiddenContainer.classList = 'd-none') {
+        document.getElementById('detailsInfo').style.overflow = 'unset';
+        document.getElementById('backlogRow').style.lineHeight = 'unset';
+        document.getElementById('backlogRow').style.maxHeight = 'unset';
+        document.getElementById('secondGap').style.alignItems = 'center';
+        hiddenContainer.classList.remove('d-none');
+        hiddenContainer.classList.add('addToBoardContainer');
+    } else {
+        hiddenContainer.classList.add('d-none');
+        hiddenContainer.classList.remove('addToBoardContainer');
+        document.getElementById('detailsInfo').style.overflow = 'hidden';
+        document.getElementById('backlogRow').style.lineHeight = '60px';
+        document.getElementById('backlogRow').style.maxHeight = '60px';
+        document.getElementById('secondGap').style.alignItems = 'center';
+
+    }
+
+}
+
+// function show() {
+//     document.getElementById('detailsInfo').style.overflow = 'unset';
+//     document.getElementById('backlogRow').style.lineHeight = 'unset';
+//     document.getElementById('backlogRow').style.maxHeight = 'unset';
+//     document.getElementById('secondGap').style.alignItems = 'center';
+//     hiddenContainer.classList.remove('d-none');
+//     hiddenContainer.classList.add('addToBoardContainer');
+// }
+
+// function hide() {
+//     hiddenContainer.classList.add('d-none');
+//     hiddenContainer.classList.remove('addToBoardContainer');
+//     document.getElementById('detailsInfo').style.overflow = 'hidden';
+//     document.getElementById('backlogRow').style.lineHeight = '60px';
+//     document.getElementById('backlogRow').style.maxHeight = '60px';
+//     document.getElementById('secondGap').style.alignItems = 'center';
+// }
