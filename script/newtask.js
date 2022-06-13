0d84139ee8fb4f7a969b67b08d2f73372f285522
RobertSebastian28 <you@example.com>
2022-06-13 16:13:53 +0200

    addTask 4

--- script/newtask.js
+++ script/newtask.js
@@ -1,1 +1,31 @@
 let allTasks = [];
+
+function addToAllTasks() {
+    let title = document.getElementById('add-task-title');
+    let dueDate = document.getElementById('add-task-dueDate');
+    let categoryA = document.getElementById("add-task-category");
+    let category = categoryA.options[categoryA.selectedIndex].text;
+    let urgencyA = document.getElementById("add-task-urgency");
+    let urgency = urgencyA.options[urgencyA.selectedIndex].text;
+    let description = document.getElementById('add-task-description');
+
+    let tasks = {
+       "title": title.value,
+       "dueDate": dueDate.value,
+       "category": category,
+       "urgency": urgency,
+       "description": description.value
+    };
+
+    allTasks.push(tasks);
+
+    deleteInputValue();
+}
+
+function deleteInputValue() { // durch <form> vieleicht nicht notwendig
+    document.getElementById('add-task-title').value = '';
+    document.getElementById('add-task-dueDate').value = '';
+    document.getElementById("add-task-category").selectedIndex = 0;
+    document.getElementById("add-task-urgency").selectedIndex = 0;
+    document.getElementById('add-task-description').value = '';
+}
