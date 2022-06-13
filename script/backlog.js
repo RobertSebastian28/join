350888a72f122bbf55df952307261bea2ba6fa45
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-06-13 15:59:39 +0200

    transfered html-code to script file

--- script/backlog.js
+++ script/backlog.js
@@ -8,3 +8,20 @@ function responsiveHead() {
         document.getElementById('testId').classList.add('d-flex');
     }
 }
+
+//render the data from json to backlog//
+function renderBacklog() {
+    let container = document.getElementById('backlogBox');
+
+    container.innerHTML = `<div class="d-flex justify-content-start rowStyle">
+    <div class="d-flex align-items-center firstGap">
+        
+        <img src="img/user_dummy.png" class="profilePicture" id="profilePicture" alt="">
+        <div id="assignedToInfo">Beispiel1</div>
+    </div>
+    <div id="testId" class="d-flex secondGap">
+        <div id="categoryInfo">Beispiel1</div>
+        <div id="detailsInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo perferendis pariatur architecto. Saepe at voluptatem dolorum harum expedita. Dignissimos aperiam dolorem consequuntur est. Doloremque exercitationem adipisci
+            et officia sequi.</div>
+    </div>`
+}
