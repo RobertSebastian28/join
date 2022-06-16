15734ddc7d304e573935b7a718889509bff5641e
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-06-16 11:22:25 +0200

    backlog load JSON + certain upgrading

--- script/backlog.js
+++ script/backlog.js
@@ -1,12 +1,12 @@
-let testData = [{
-    "title": "test1",
-    "description": "blablabla1",
-    "board": "backlog",
-    "dueDate": 0,
-    "category": "Marketing",
-    "urgency": "high",
-    "assignedTo": "no one"
-}]
+// let testData = [{
+//     "title": "test1",
+//     "description": "blablabla1",
+//     "board": "backlog",
+//     "dueDate": 0,
+//     "category": "Marketing",
+//     "urgency": "high",
+//     "assignedTo": "no one"
+// }]
 
 const colors = {
     high: '#ff0000',
@@ -30,89 +30,80 @@ function responsiveHead() {
 function renderBacklog() {
     let container = document.getElementById('backlogBox');
 
-    let assignedTo = testData[0]['assignedTo'];
-    let category = testData[0]['category'];
-    let details = testData[0]['description'];
-    let priority = testData[0]['urgency'];
-    let color = colors[priority.toLowerCase()];
+    for (let i = 0; i < data.length; i++) {
+        const task = data[i];
 
-    container.innerHTML += `
+        let assignedTo = task['assignedTo'];
+        let category = task['category'];
+        let details = task['description'];
+        let priority = task['urgency'];
+        let color = colors[priority.toLowerCase()];
+
+        container.innerHTML += `
     
-    <div id="backlogRow" onclick="showTask()" class="d-flex justify-content-start rowStyle" style="border-color: ${color}">
+    <div id="backlogRow${i}" onclick="showTask(${i})" class="d-flex justify-content-start rowStyle" style="border-color: ${color}">
         <div class="d-flex align-items-center firstGap">
             
-        <img src="img/user_dummy.png" class="profilePicture" id="profilePicture" alt="">
+        <img src="img/user_dummy.png" class="profilePicture" id="profilePicture${i}" alt="">
             <div id="assignedToInfo">${assignedTo}</div>
         </div>
-        <div id="secondGap" class="d-flex secondGap">
-            <div id="categoryInfo">${category}</div>
-            <div id="detailsInfo">${details}</div>
+        <div id="secondGap${i}" class="d-flex secondGap">
+            <div id="categoryInfo${i}">${category}</div>
+            <div id="detailsInfo${i}">${details}</div>
         </div>
     </div>
-        <div id="addToBoard" style="display:none; border-color: ${color}">
-            <div class="section">Send to Board</div>
+        <div id="addToBoard${i}" style="display:none; border-color: ${color}">
+            <div class="section" onclick="sendTaskToBoard(${task})">Send to Board</div>
     <div>
     `
-}
-
-/////////////function for show & hide the full task details////////////
-function showTask() {
-    let hiddenContainer = document.getElementById('addToBoard');
-
-    if (hiddenContainer.style.display == 'none') {
-        show();
-    } else {
-        hide();
     }
 }
 
-function show() {
-    let hiddenContainer = document.getElementById('addToBoard');
 
-    document.getElementById('detailsInfo').style.overflow = 'unset';
-    document.getElementById('backlogRow').style.lineHeight = 'unset';
-    document.getElementById('backlogRow').style.maxHeight = 'unset';
-    document.getElementById('secondGap').style.alignItems = 'center';
-    document.getElementById('backlogRow').style.marginBottom = 'unset';
-    document.getElementById('backlogRow').style.paddingBottom = '8px';
-    document.getElementById('backlogRow').style.borderRadius = '5px 5px 0px 0px';
+/////////////send the selected task to the board/////////////////////
+function sendTaskToBoard(task) {
+
+
+
+}
+
+
+/////////////function for show & hide the full task details////////////
+function showTask(i) {
+    let hiddenContainer = document.getElementById('addToBoard' + i);
+
+    if (hiddenContainer.style.display == 'none') {
+        show(i);
+    } else {
+        hide(i);
+    }
+}
+
+function show(i) {
+    let hiddenContainer = document.getElementById('addToBoard' + i);
+
+    document.getElementById('detailsInfo' + i).style.overflow = 'unset';
+    document.getElementById('backlogRow' + i).style.lineHeight = 'unset';
+    document.getElementById('backlogRow' + i).style.maxHeight = 'unset';
+    document.getElementById('secondGap' + i).style.alignItems = 'center';
+    document.getElementById('backlogRow' + i).style.marginBottom = 'unset';
+    document.getElementById('backlogRow' + i).style.paddingBottom = '8px';
+    document.getElementById('backlogRow' + i).style.borderRadius = '5px 5px 0px 0px';
 
     hiddenContainer.style.display = 'flex';
     hiddenContainer.classList.add('addToBoardContainer');
 }
 
-function hide() {
-    let hiddenContainer = document.getElementById('addToBoard');
+function hide(i) {
+    let hiddenContainer = document.getElementById('addToBoard' + i);
 
     hiddenContainer.classList.remove('addToBoardContainer');
     hiddenContainer.style.display = 'none';
-    document.getElementById('backlogRow').style.lineHeight = '60px';
-    document.getElementById('backlogRow').style.maxHeight = '60px';
-    document.getElementById('secondGap').style.alignItems = 'unset';
-    document.getElementById('backlogRow').style.marginBottom = '8px';
-    document.getElementById('backlogRow').style.paddingBottom = 'unset';
-    document.getElementById('detailsInfo').style.overflow = 'hidden';
-    document.getElementById('backlogRow').style.borderRadius = '5px';
+    document.getElementById('backlogRow' + i).style.lineHeight = '60px';
+    document.getElementById('backlogRow' + i).style.maxHeight = '60px';
+    document.getElementById('secondGap' + i).style.alignItems = 'unset';
+    document.getElementById('backlogRow' + i).style.marginBottom = '8px';
+    document.getElementById('backlogRow' + i).style.paddingBottom = 'unset';
+    document.getElementById('detailsInfo' + i).style.overflow = 'hidden';
+    document.getElementById('backlogRow' + i).style.borderRadius = '5px';
 }
-
-
-
-
-
-// /**
-//  * saves to backend.   // has to be checked if it works (note sure if right)
-//  */
-// function saveAtBackend() {
-//     let dataToText = JSON.stringify(data);
-//     backend.setItem('data', dataToText);
-// }
-
-// /**
-//  * loads from backend.   // has to be checked if it works (note sure if right)
-//  */
-// function loadFromBackend() {
-//     let textToData = backend.getItem('data');
-//     if (textToData) {
-//         data = JSON.parse(textToData);
-//     }
-// }
